f6099ee5fb74c3976188bb35b4d776c38ed78163
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-02-05 03:00:13 +0100

    save it before loose it

--- src/pages/Project_sub.js
+++ src/pages/Project_sub.js
@@ -1,8 +1,17 @@
 import projects from "./projects.json";
 
 
+import { useState } from 'react';
+
 const Project_sub = () => {
-  
+  const [showDescription, setShowDescription] = useState({});
+
+  const handleClick = (title) => {
+    const newShowDescription = {...showDescription};
+    newShowDescription[title] = !newShowDescription[title];
+    setShowDescription(newShowDescription);
+  }
+
   return (
     <>
       {projects.map(item => {
@@ -10,9 +19,13 @@ const Project_sub = () => {
           <div className="projects" key={item.title}>
             <div className="cards">
             <h1>{item.title}</h1>
-            <p><a href="{item.website}" target="_blank">Visit Project</a></p>
+            <p><a href="{item.website}">Visit Project</a></p>
             <p><a href="{item.github}">GitHub</a></p>
-            <img src={item.image} alt={item.title} />
+            <img src={item.image} alt={item.title} /><br></br>
+            {showDescription[item.title] ? <p>{item.description}</p> : null}
+            <button onClick={() => handleClick(item.title)}>
+              {showDescription[item.title] ? 'Hide Description' : 'Show Description'}
+            </button>
             </div>
           </div>
         );
